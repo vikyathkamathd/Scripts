faf797a723f7bdfe91c8eecbe95c8959e22e7b0f
vikyathkamathd <84204445+vikyathkamathd@users.noreply.github.com>
2024-03-08 18:42:43 +0530

    Remove vnswap and zswap

--- zram.sh
+++ zram.sh
@@ -1,7 +1,10 @@
+#1Gb zram
 echo 1G > /sys/block/zram0/disksize
+#remove vnswap
 swapoff /dev/block/vnswap0
 mkswap /dev/block/zram0
 sleep 5
 swapon /dev/block/zram0
+#disable zswap
 chmod 644 /sys/module/zswap/parameters/enabled
 echo N > /sys/module/zswap/parameters/enabled
